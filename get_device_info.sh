81f9e9ce8ace26fb0fbd3ae57d9eba9861549f3e
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-09-26 13:57:36 +0600

    Update STOCK_DVFS_FILENAME extraction logic

--- get_device_info.sh
+++ get_device_info.sh
@@ -41,7 +41,8 @@ GET_SIOP_DVFS_FILE_NAME() {
         return 1
     fi
 
-    export STOCK_DVFS_FILENAME=$(grep -oE '"dvfs_policy_[^"]*"' "$FILE" | head -n1 | tr -d '"')
+    export STOCK_DVFS_FILENAME=$(grep -E 'const-string [vp][2-9]+, "dvfs_policy_.*"' "$FILE")
+    export STOCK_DVFS_FILENAME=$(echo "$STOCK_DVFS_FILENAME" | sed -E 's/.*(dvfs_policy_.*)/\1/')
     export STOCK_SIOP_POLICY_FILENAME=$(grep -oE '"siop_[^"]*_[^"]*"' "$FILE" | head -n1 | tr -d '"')
 }
 
